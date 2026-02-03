7b1f88a17e3d079fca34d7fd8b368b6423130153
Venkatesh <venkateshchakrabarty@gmail.com>
2026-02-03 23:13:06 +0530

    add:frontend set up space page and card | fetch space data

---
+++ frontend/app/space/page.tsx
@@ -0,0 +1,44 @@
+"use client";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { Space } from "@/components/space/types/SpaceTypes";
+import SpaceCard from "@/components/space/types/SpaceCard";
+import { LargeText } from "@/components/reusables/texts/Texts";
+
+const SpacePage = () => {
+  const baseURL = process.env.NEXT_PUBLIC_API_BASE_URL;
+  const [spaces, setSpaces] = useState([]);
+
+  useEffect(() => {
+    const fetchSpaces = async () => {
+      const response = await axios.get(`${baseURL}/spaces`, {
+        params: {
+          userId: "4e9b79b4-c7e9-4d04-b709-92feafebacdf",
+        },
+      });
+      setSpaces(response.data);
+    };
+    fetchSpaces();
+  }, []);
+
+  return (
+    <div>
+      <LargeText>Your spaces</LargeText>
+      <div className='flex flex-wrap gap-12 my-6'>
+        {spaces.map((space: Space) => (
+          <SpaceCard
+            key={space.id}
+            id={space.id}
+            name={space.name}
+            description={space.description}
+            imageURL={
+              "https://images.unsplash.com/photo-1624725412168-a8e69d4f7b36?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+            }
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default SpacePage;
---
+++ frontend/components/space/types/SpaceTypes.ts
@@ -0,0 +1,6 @@
+export type Space = {
+  id: string
+  name: string
+  description: string
+  imageURL: string
+}
