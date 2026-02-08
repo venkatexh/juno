9f4860a1bd7a5d24072ae0c0b5e05bd100a1915c
Venkatesh <venkateshchakrabarty@gmail.com>
2026-02-08 15:37:23 +0530

    update:frontend app navbars

--- frontend/components/navbar/NavbarAppSecondary.tsx
+++ frontend/components/navbar/NavbarAppSecondary.tsx
@@ -1,8 +1,25 @@
+"use client";
+
+import { menuItemsNavSec } from "@/resources/constants/navbar/MenuItemsNavbarSecondary";
+import { NavItem } from "@/resources/constants/navbar/types/NavItem";
+import Link from "next/link";
+import { useParams } from "next/navigation";
+
 const NavbarAppSecondary = () => {
+  const params = useParams();
+
   return (
-    <div className='fixed left-16 w-64 h-screen border-r border-slate-800'>
-      Sec
+    <div className='fixed left-16 w-64 h-screen border-r border-slate-800 flex flex-col py-10'>
+      {menuItemsNavSec.map((item: NavItem) => (
+        <Link
+          key={item.title}
+          className='my-1 mx-6 text-lg'
+          href={`/space/${params?.id}/${item.href}`}>
+          <div>{item.title}</div>
+        </Link>
+      ))}
     </div>
   );
 };
+
 export default NavbarAppSecondary;
---
+++ frontend/resources/constants/navbar/MenuItemsNavbarSecondary.ts
@@ -0,0 +1,17 @@
+export const menuItemsNavSec = [
+  {
+    title: "Home",
+    href: "/",
+    icon: "nav_home",
+  },
+  {
+    title: "Links",
+    href: "links",
+    icon: "nav_links",
+  },
+  {
+    title: "Polls",
+    href: "/polls",
+    icon: "nav_polls",
+  },
+];
