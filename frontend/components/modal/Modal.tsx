4cb5d8c4f30afa87e46f36928c677edf301d63ab
Venkatesh <venkateshchakrabarty@gmail.com>
2026-02-09 13:53:58 +0530

    update:frontend modal component

--- frontend/components/modal/Modal.tsx
+++ frontend/components/modal/Modal.tsx
@@ -13,10 +13,14 @@ const Modal = ({
     isOpen && (
       <div className='fixed w-screen h-screen top-0 left-0 bg-black/30 bg-opacity-50 z-40'>
         <div
-          className='absolute w-[50vw] h-[50vh] top-0 left-0 right-0 bottom-0 m-auto bg-black flex 
-          items-center justify-center rounded-2xl z-50'>
-          <div onClick={onClose}>close</div>
+          className='absolute w-[50vw] max-h-[50vh] top-0 left-0 right-0 bottom-0 m-auto px-12 py-6 
+          flex flex-col bg-black rounded-2xl z-50'>
           {children}
+          <div
+            className='absolute top-4 right-4 cursor-pointer'
+            onClick={onClose}>
+            close
+          </div>
         </div>
       </div>
     )
