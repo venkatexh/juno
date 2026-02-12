c241220d36bae9fa72088fd9aa774a36ffb88d47
Venkatesh <venkateshchakrabarty@gmail.com>
2026-02-12 19:14:00 +0530

    update:frontend add onclick to text components

--- frontend/components/reusables/texts/Texts.tsx
+++ frontend/components/reusables/texts/Texts.tsx
@@ -1,19 +1,35 @@
 import { Text } from "@/components/reusables/texts/types/Text";
 
-const TextSmall = ({ children, className }: Text) => {
-  return <p className={`${className} text-sm text-gray-400`}>{children}</p>;
+const TextSmall = ({ children, className, onClick }: Text) => {
+  return (
+    <p className={`${className} text-sm text-gray-400`} onClick={onClick}>
+      {children}
+    </p>
+  );
 };
 
-const TextMedium = ({ children, className }: Text) => {
-  return <p className={`${className} text-base text-gray-400`}>{children}</p>;
+const TextMedium = ({ children, className, onClick }: Text) => {
+  return (
+    <p className={`${className} text-base text-gray-400`} onClick={onClick}>
+      {children}
+    </p>
+  );
 };
 
-const TextLarge = ({ children, className }: Text) => {
-  return <p className={`${className} text-lg text-gray-400`}>{children}</p>;
+const TextLarge = ({ children, className, onClick }: Text) => {
+  return (
+    <p className={`${className} text-lg text-gray-400`} onClick={onClick}>
+      {children}
+    </p>
+  );
 };
 
-const TextHeading = ({ children, className }: Text) => {
-  return <p className={`${className} text-3xl text-gray-400`}>{children}</p>;
+const TextHeading = ({ children, className, onClick }: Text) => {
+  return (
+    <p className={`${className} text-3xl text-gray-400`} onClick={onClick}>
+      {children}
+    </p>
+  );
 };
 
 export { TextSmall, TextMedium, TextLarge, TextHeading };
--- frontend/components/reusables/texts/types/Text.ts
+++ frontend/components/reusables/texts/types/Text.ts
@@ -1,4 +1,5 @@
 export type Text = {
   children: string | string[];
   className?: string;
+  onClick?: () => void;
 };
