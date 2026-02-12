af1dd76d54ec8e376b409f83ce58feadd80e5158
Venkatesh <venkateshchakrabarty@gmail.com>
2026-02-12 22:24:16 +0530

    add:frontend module creation form submission

--- frontend/components/module/CreateModuleForm.tsx
+++ frontend/components/module/CreateModuleForm.tsx
@@ -1,26 +1,53 @@
 import axios from "axios";
-import { GeneralButton } from "../reusables/buttons/Buttons";
-import { TextLarge } from "../reusables/texts/Texts";
+import { useParams } from "next/navigation";
 import { useEffect, useState } from "react";
+
 import TemplateCard from "./TemplateCard";
+import { TextLarge } from "@/components/reusables/texts/Texts";
+import { Input } from "@/components/reusables/form-elements/Inputs";
+import { SubmitButton } from "@/components/reusables/buttons/Buttons";
+
 import { TemplateFields } from "./types/TemplateFields";
 
 const CreateModuleForm = () => {
+  const params = useParams();
   const baseURL = process.env.NEXT_PUBLIC_API_BASE_URL;
 
+  const [moduleName, setModuleName] = useState("");
+  const [selectedTemplateId, setSelectedTemplateId] = useState("");
   const [templates, setTemplates] = useState<TemplateFields[]>([]);
-  const [selectedTemplate, setSelectedTemplate] = useState("");
 
   useEffect(() => {
     const fetchTemplates = async () => {
-      const response = await axios.get(`${baseURL}/modules/templates`);
-      setTemplates(response.data);
+      const res = await axios.get(`${baseURL}/modules/templates`);
+      if (res.status === 200 || res.status === 201) {
+        setTemplates(res.data);
+      }
     };
 
     fetchTemplates();
-  });
+  }, [baseURL]);
 
-  const handleNextClick = () => {};
+  const handleFormSubmit = () => {
+    const moduleBody = {
+      name: moduleName,
+      templateId: selectedTemplateId,
+      spaceId: params?.id,
+      // Hard coded
+      createdByUserId: "207aaca1-3904-4d70-a342-760fdb73199d",
+    };
+
+    axios
+      .post(`${baseURL}/modules`, moduleBody)
+      .then((res) => {
+        if (res.status === 201 || res.status === 200) {
+          console.log(res.data);
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
 
   return (
     <div className='w-full h-full flex flex-col justify-between'>
@@ -35,17 +62,25 @@ const CreateModuleForm = () => {
               description={template.description}
               subscriptionType={template.subscriptionType}
               imageURL={template.imageURL}
-              handleSetSelectedTemplate={(id) => setSelectedTemplate(id)}
-              selected={selectedTemplate === template.id}
+              handleSetSelectedTemplate={(id) => setSelectedTemplateId(id)}
+              selected={selectedTemplateId === template.id}
             />
           ))}
         </div>
       </div>
-      <div className='flex'>
-        <GeneralButton
-          text='Next'
-          className='ml-auto'
-          onClick={() => handleNextClick()}
+      <div className='w-full flex items-center justify-between'>
+        <Input
+          type='text'
+          name='name'
+          placeholder='Enter module name'
+          onChange={(e) => setModuleName(e.target.value)}
+          value={moduleName}
+          maxLength={20}
+        />
+        <SubmitButton
+          text='Create'
+          className='ml-12'
+          onClick={() => handleFormSubmit()}
         />
       </div>
     </div>
